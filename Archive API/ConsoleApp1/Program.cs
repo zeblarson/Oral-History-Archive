16aca4c60f76452c7ad8e4b5b0a7c11e7e6864f8
Zeb Larson <zeb.larson@gmail.com>
2020-06-27 16:44:41 -0400

    Begun work on API code, added Interview Model

--- Archive API/ConsoleApp1/Program.cs
+++ Archive API/ConsoleApp1/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.
 
 namespace ConsoleApp1
 {
---
+++ Archive API/WebApplication1/Models/Interview.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Interview
+    {
+        public int? Id { get; set; }
+        public string Title {get; set;}
+        public string InterviewerName { get; set; }
+        public string IntervieweeName { get; set; }
+        public DateTime DateConducted { get; set; }
+        public bool TranscriptAvailable { get; set; }
+        public string Summary { get; set; }
+
+        public Interview ()
+        {
+
+        }
+        
+        public bool IsValid
+        {
+            get
+            {
+                return Id != 0 && Title != null && InterviewerName != null && IntervieweeName != null && DateConducted != null && Summary != null;
+            }
+        }
+    }
+}
